25b88318ebc175867100ba540f282ab8ae0db632
Senay Inan <senayinan@Senays-MacBook-Pro.local>
2023-03-02 23:10:12 -0400

    First Commit

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // TODO: add code here
 window.addEventListener("load", function() {
-    this.fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response) {
-   
-   return response.json();
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+    return response.json();
+    
 }).then(function(response) {
+   
+    
+
+    
     let sortedAstronauts = response.sort(function(a,b){return a.hoursInSpace - b.hoursInSpace})
     let myContainer = document.getElementById("container");
     myContainer.innerHTML +=`<h3>Astronaut Count: ${sortedAstronauts.length}</h3>`;
     sortedAstronauts.forEach(function(astronaut) {
         let myHtml = `
-        <div class= "bio">
+        <div class= "astronaut">
             <div class= "bio">
                 <h3>${astronaut.firstName} ${astronaut.lastName}</h3
                 <ul>
